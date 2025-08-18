48796cc48755e0ad7764287f064e3fe10ed0dfa4
mp40 <philps.matthew@gmail.com>
2025-08-18 21:32:55 +0200

    add account page to render

--- render/render.go
+++ render/render.go
@@ -44,182 +44,34 @@ func (r *Render) RenderHomeFragment(w io.Writer) error {
 }
 
 func (r *Render) RenderAccountPage(w io.Writer) error {
+	tmpl, err := template.ParseFiles(getTemplatePath("index.html"), getTemplatePath("account.html"))
+
+	if err != nil {
+		return err
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (r *Render) RenderAccountFragment(w io.Writer) error {
+	tmpl, err := template.New("page").ParseFiles(getTemplatePath("account.html"))
+
+	if err != nil {
+		return err
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
-
-// func RenderCharactersPage(w io.Writer) error {
-// 	tmpl, err := template.ParseFiles(getTemplatePath("index.html"), getTemplatePath("characters.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderCharactersFragment(w io.Writer) error {
-// 	tmpl, err := template.New("page").ParseFiles(getTemplatePath("characters.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderToolsPage(w io.Writer) error {
-// 	tmpl, err := template.ParseFiles(getTemplatePath("index.html"), getTemplatePath("tools.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderToolsFragment(w io.Writer) error {
-// 	tmpl, err := template.New("page").ParseFiles(getTemplatePath("tools.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderToolsShootingPage(w io.Writer) error {
-// 	tmpl, err := template.ParseFiles(
-// 		getTemplatePath("index.html"),
-// 		getTemplatePath("tools-shooting.html"),
-// 		getTemplatePath("tools-shooting-eal.html"),
-// 	)
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderToolsShootingFragment(w io.Writer) error {
-// 	tmpl, err := template.New("page").ParseFiles(getTemplatePath("tools-shooting.html"), getTemplatePath("tools-shooting-eal.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderToolsShootingCalculatorFragment(w io.Writer) error {
-// 	tmpl, err := template.New("calculator").ParseFiles(getTemplatePath("tools-shooting-eal.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderToolsShotgunsPage(w io.Writer) error {
-// 	tmpl, err := template.ParseFiles(getTemplatePath("index.html"), getTemplatePath("tools-shotguns.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderToolsShotgunsFragment(w io.Writer) error {
-// 	tmpl, err := template.New("page").ParseFiles(getTemplatePath("tools-shotguns.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderToolsHandToHandPage(w io.Writer) error {
-// 	tmpl, err := template.ParseFiles(getTemplatePath("index.html"), getTemplatePath("tools-hand-to-hand.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func RenderToolsHandToHandFragment(w io.Writer) error {
-// 	tmpl, err := template.New("page").ParseFiles(getTemplatePath("tools-hand-to-hand.html"))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
 
 func getTemplatePath(templateName string) string {
 	_, b, _, _ := runtime.Caller(0)
