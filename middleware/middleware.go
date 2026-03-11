6d3cf1a50b046e32023f1b4a9df3f85d7f4335b5
mp40 <philps.matthew@gmail.com>
2026-03-11 19:27:29 +0100

    flesh out auth middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -23,41 +23,51 @@ func NewMiddlewareService(data Data) *Middleware {
 
 func (m *Middleware) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		c := r.Context()
-		ctx := context.WithValue(c, "Test", "123")
-
 		cookie, err := r.Cookie("userID")
 		if err != nil {
-			// not autherised || internal server error
+			// GREY - for now fall through
+			next.ServeHTTP(w, r)
+			return
 		}
 		if cookie == nil {
-			// not signed in or authorised
+			// definate NO
+			next.ServeHTTP(w, r)
+			return
 		}
-		if cookie != nil {
-			rawID := cookie.Value
-			userID, err := uuid.Parse(rawID)
-			if err != nil {
-				// not autherised || internal server error
-			}
-
-			// get count by id
-			count, err := m.data.CountUserByID(userID)
-			if err != nil {
-				// not autherised || internal server error
-			}
-			if count > 1 {
-				// ID is unique
-				// not autherised || internal server error
-			}
-			if count == 0 {
-				// we set cookie so if no matching ID?
-				// not autherised || internal server erro
-			}
-
-			// if user count = 1 authorised and signed in
+		rawID := cookie.Value
+		userID, err := uuid.Parse(rawID)
+		if err != nil {
+			// GREY if we set cookie, then we expect parseable id
+			// for now fall through - latter decide other action ie redirect to sign in with err msg
+			next.ServeHTTP(w, r)
+			return
 		}
+
+		// get count by id
+		count, err := m.data.CountUserByID(userID)
+		if err != nil {
+			// GREY data layer error - do not know correct count
+			// for now fall through
+			next.ServeHTTP(w, r)
+			return
+		}
+		if count > 1 {
+			// GREY if we set cookie, then we expect 1
+			// for now fall through - latter decide other action ie redirect to sign in
+			next.ServeHTTP(w, r)
+			return
+		}
+		if count == 0 {
+			// GREY if we set cookie, then we expect 1
+			// for now fall through
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// if user count = 1, def YES
+		c := r.Context()
+		ctx := context.WithValue(c, "userID", userID)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
-		// return
 	})
 }
--- middleware/middleware_test.go
+++ middleware/middleware_test.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -19,22 +20,24 @@ func (d *StubData) CountUserByID(ID uuid.UUID) (int, error) {
 	return d.count, d.err
 }
 
-func TestSignUp(t *testing.T) {
-	t.Run("it should not panic", func(t *testing.T) {
+func TestMiddleware(t *testing.T) {
+	t.Run("it should handle cookie with uuid as user id", func(t *testing.T) {
 		stubData := StubData{}
 		stubData.count = 1
 		middleware := NewMiddlewareService(&stubData)
 
 		ID := uuid.MustParse("5ea69240-823c-4523-90a2-4868a5bfc90a")
 
+		var contextUserId *uuid.UUID
 		nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			raw, ok := r.Context().Value("userID").(uuid.UUID)
+			if ok {
+				contextUserId = &raw
+			}
 			w.WriteHeader(http.StatusOK)
 		})
 
 		handler := middleware.AuthMiddleware(nextHandler)
-		s := httptest.NewServer(handler)
-		defer s.Close()
-
 		request := httptest.NewRequest("GET", "/", nil)
 		request.AddCookie(&http.Cookie{Name: "userID", Value: ID.String()})
 
@@ -47,6 +50,187 @@ func TestSignUp(t *testing.T) {
 
 		if response.Code != http.StatusOK {
 			t.Errorf("unexpected status, got: %v", response.Code)
+		}
+
+		if contextUserId == nil {
+			t.Errorf("expected user uuid in context, got nil")
+		}
+
+		if *contextUserId != ID {
+			t.Errorf("unexpected user uuid in context, got %v", contextUserId)
+		}
+	})
+
+	t.Run("it should handle cookie with unparsable user id", func(t *testing.T) {
+		stubData := StubData{}
+		middleware := NewMiddlewareService(&stubData)
+
+		var contextUserId *uuid.UUID
+		nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			raw, ok := r.Context().Value("userID").(uuid.UUID)
+			if ok {
+				contextUserId = &raw
+			}
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler := middleware.AuthMiddleware(nextHandler)
+		request := httptest.NewRequest("GET", "/", nil)
+		request.AddCookie(&http.Cookie{Name: "userID", Value: "whoops"})
+
+		response := httptest.NewRecorder()
+		handler.ServeHTTP(response, request)
+
+		if stubData.spyCountUser != 0 {
+			t.Errorf("want 1 call to data, got: %v", stubData.spyCountUser)
+		}
+
+		if response.Code != http.StatusOK {
+			t.Errorf("unexpected status, got: %v", response.Code)
+		}
+
+		if contextUserId != nil {
+			t.Errorf("unexpected user uuid in context, got %v", *contextUserId)
+		}
+	})
+
+	t.Run("it should handle requests without cookie", func(t *testing.T) {
+		stubData := StubData{}
+		middleware := NewMiddlewareService(&stubData)
+
+		var contextUserId *uuid.UUID
+		nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			raw, ok := r.Context().Value("userID").(uuid.UUID)
+			if ok {
+				contextUserId = &raw
+			}
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler := middleware.AuthMiddleware(nextHandler)
+		request := httptest.NewRequest("GET", "/", nil)
+
+		response := httptest.NewRecorder()
+		handler.ServeHTTP(response, request)
+
+		if stubData.spyCountUser != 0 {
+			t.Errorf("want 0 call to data, got: %v", stubData.spyCountUser)
+		}
+
+		if response.Code != http.StatusOK {
+			t.Errorf("unexpected status, got: %v", response.Code)
+		}
+
+		if contextUserId != nil {
+			t.Errorf("unexpected user uuid in context, got %v", *contextUserId)
+		}
+	})
+
+	t.Run("it should handle user id with count 0", func(t *testing.T) {
+		stubData := StubData{}
+		stubData.count = 0
+		middleware := NewMiddlewareService(&stubData)
+
+		ID := uuid.MustParse("5ea69240-823c-4523-90a2-4868a5bfc90a")
+
+		var contextUserId *uuid.UUID
+		nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			raw, ok := r.Context().Value("userID").(uuid.UUID)
+			if ok {
+				contextUserId = &raw
+			}
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler := middleware.AuthMiddleware(nextHandler)
+		request := httptest.NewRequest("GET", "/", nil)
+		request.AddCookie(&http.Cookie{Name: "userID", Value: ID.String()})
+
+		response := httptest.NewRecorder()
+		handler.ServeHTTP(response, request)
+
+		if stubData.spyCountUser != 1 {
+			t.Errorf("want 1 call to data, got: %v", stubData.spyCountUser)
+		}
+
+		if response.Code != http.StatusOK {
+			t.Errorf("unexpected status, got: %v", response.Code)
+		}
+
+		if contextUserId != nil {
+			t.Errorf("unexpected user uuid in context, got %v", *contextUserId)
+		}
+	})
+
+	t.Run("it should handle user id with count greater than 1", func(t *testing.T) {
+		stubData := StubData{}
+		stubData.count = 2
+		middleware := NewMiddlewareService(&stubData)
+
+		ID := uuid.MustParse("5ea69240-823c-4523-90a2-4868a5bfc90a")
+
+		var contextUserId *uuid.UUID
+		nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			raw, ok := r.Context().Value("userID").(uuid.UUID)
+			if ok {
+				contextUserId = &raw
+			}
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler := middleware.AuthMiddleware(nextHandler)
+		request := httptest.NewRequest("GET", "/", nil)
+		request.AddCookie(&http.Cookie{Name: "userID", Value: ID.String()})
+
+		response := httptest.NewRecorder()
+		handler.ServeHTTP(response, request)
+
+		if stubData.spyCountUser != 1 {
+			t.Errorf("want 1 call to data, got: %v", stubData.spyCountUser)
+		}
+
+		if response.Code != http.StatusOK {
+			t.Errorf("unexpected status, got: %v", response.Code)
+		}
+
+		if contextUserId != nil {
+			t.Errorf("unexpected user uuid in context, got %v", *contextUserId)
+		}
+	})
+
+	t.Run("it should handle errors from count user", func(t *testing.T) {
+		stubData := StubData{}
+		stubData.err = fmt.Errorf("fake-err")
+		middleware := NewMiddlewareService(&stubData)
+
+		ID := uuid.MustParse("5ea69240-823c-4523-90a2-4868a5bfc90a")
+
+		var contextUserId *uuid.UUID
+		nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			raw, ok := r.Context().Value("userID").(uuid.UUID)
+			if ok {
+				contextUserId = &raw
+			}
+			w.WriteHeader(http.StatusOK)
+		})
+
+		handler := middleware.AuthMiddleware(nextHandler)
+		request := httptest.NewRequest("GET", "/", nil)
+		request.AddCookie(&http.Cookie{Name: "userID", Value: ID.String()})
+
+		response := httptest.NewRecorder()
+		handler.ServeHTTP(response, request)
+
+		if stubData.spyCountUser != 1 {
+			t.Errorf("want 1 call to data, got: %v", stubData.spyCountUser)
+		}
+
+		if response.Code != http.StatusOK {
+			t.Errorf("unexpected status, got: %v", response.Code)
+		}
+
+		if contextUserId != nil {
+			t.Errorf("unexpected user uuid in context, got %v", *contextUserId)
 		}
 	})
 }
