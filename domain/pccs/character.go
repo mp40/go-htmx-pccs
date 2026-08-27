d78ba5dcc39f14de26711c988c19461d9cc68e95
mp40 <philps.matthew@gmail.com>
2026-08-27 15:45:34 +0200

    refactor levels and learning points to single source of truth

--- domain/pccs/character.go
+++ domain/pccs/character.go
@@ -25,48 +25,11 @@ var LevelToLearningPoints = map[int]float32{
 }
 
 func ConvertLearningPointsToLevel(learningPoints float32) int {
-	switch {
-	case learningPoints < 2:
-		return 0
-	case learningPoints < 4:
-		return 1
-	case learningPoints < 8:
-		return 2
-	case learningPoints < 16:
-		return 3
-	case learningPoints < 32:
-		return 4
-	case learningPoints < 56:
-		return 5
-	case learningPoints < 88:
-		return 6
-	case learningPoints < 126:
-		return 7
-	case learningPoints < 170:
-		return 8
-	case learningPoints < 218:
-		return 9
-	case learningPoints < 274:
-		return 10
-	case learningPoints < 346:
-		return 11
-	case learningPoints < 434:
-		return 12
-	case learningPoints < 542:
-		return 13
-	case learningPoints < 674:
-		return 14
-	case learningPoints < 834:
-		return 15
-	case learningPoints < 1026:
-		return 16
-	case learningPoints < 1254:
-		return 17
-	case learningPoints < 1552:
-		return 18
-	case learningPoints < 1834:
-		return 19
-	default:
-		return 20
+	level := 0
+	for l := 1; l <= 20; l++ {
+		if learningPoints >= LevelToLearningPoints[l] {
+			level = l
+		}
 	}
+	return level
 }
