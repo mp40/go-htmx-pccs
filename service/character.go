30200e245909117a0d5b2a20914b475ab3d190d0
mp40 <philps.matthew@gmail.com>
2026-08-27 15:38:38 +0200

    extract domain rules to domian pccs package

--- service/character.go
+++ service/character.go
@@ -102,12 +102,6 @@ func getRandomName() string {
 	return fmt.Sprintf("%s-%d", names[i], generateRandomNumber(100, 999))
 }
 
-func convertLevelToLearningPoints(level int) float32 {
-	// TODO think if we want to return error if level not within map boundry, ie -1 or 21
-	lp := pccs.LevelToLearningPoints[level]
-	return lp
-}
-
 func mapStoreCharacterToDomainCharacter(c store.Character) domain.CharacterDTO {
 	dto := domain.CharacterDTO{
 		RawCharacter: domain.RawCharacter{
@@ -121,56 +115,8 @@ func mapStoreCharacterToDomainCharacter(c store.Character) domain.CharacterDTO {
 			GunCombatLearningPoints:  c.GunCombatLearningPoints,
 			HandToHandLearningPoints: c.HandToHandLearningPoints,
 		},
-		GunCombatLevel:  convertLearningPointsToLevel(c.GunCombatLearningPoints),
-		HandToHandLevel: convertLearningPointsToLevel(c.HandToHandLearningPoints),
+		GunCombatLevel:  pccs.ConvertLearningPointsToLevel(c.GunCombatLearningPoints),
+		HandToHandLevel: pccs.ConvertLearningPointsToLevel(c.HandToHandLearningPoints),
 	}
 	return dto
-}
-
-// move to domain/pccs
-func convertLearningPointsToLevel(learningPoints float32) int {
-	switch {
-	case learningPoints < 2:
-		return 0
-	case learningPoints < 4:
-		return 1
-	case learningPoints < 8:
-		return 2
-	case learningPoints < 16:
-		return 3
-	case learningPoints < 32:
-		return 4
-	case learningPoints < 56:
-		return 5
-	case learningPoints < 88:
-		return 6
-	case learningPoints < 126:
-		return 7
-	case learningPoints < 170:
-		return 8
-	case learningPoints < 218:
-		return 9
-	case learningPoints < 274:
-		return 10
-	case learningPoints < 346:
-		return 11
-	case learningPoints < 434:
-		return 12
-	case learningPoints < 542:
-		return 13
-	case learningPoints < 674:
-		return 14
-	case learningPoints < 834:
-		return 15
-	case learningPoints < 1026:
-		return 16
-	case learningPoints < 1254:
-		return 17
-	case learningPoints < 1552:
-		return 18
-	case learningPoints < 1834:
-		return 19
-	default:
-		return 20
-	}
 }
--- service/character_test.go
+++ service/character_test.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/google/uuid"
@@ -110,93 +109,4 @@ func TestEditCharacter(t *testing.T) {
 			t.Errorf("expected character not to be nil")
 		}
 	})
-}
-
-func TestConvertLevelToLearningPoints(t *testing.T) {
-	tc := []struct {
-		level int
-		want  float32
-	}{
-		{level: 0, want: 0},
-		{level: 1, want: 2},
-		{level: 2, want: 4},
-		{level: 3, want: 8},
-		{level: 4, want: 16},
-		{level: 5, want: 32},
-		{level: 6, want: 56},
-		{level: 7, want: 88},
-		{level: 8, want: 126},
-		{level: 9, want: 170},
-		{level: 10, want: 218},
-		{level: 11, want: 274},
-		{level: 12, want: 346},
-		{level: 13, want: 434},
-		{level: 14, want: 542},
-		{level: 15, want: 674},
-		{level: 16, want: 834},
-		{level: 17, want: 1026},
-		{level: 18, want: 1254},
-		{level: 19, want: 1552},
-		{level: 20, want: 1834},
-	}
-
-	for _, test := range tc {
-		t.Run(fmt.Sprintf("it converts level %d to %v", test.level, test.want), func(t *testing.T) {
-			t.Parallel()
-
-			got := convertLevelToLearningPoints(test.level)
-			if got != test.want {
-				t.Errorf("got %v, want %v", got, test.want)
-			}
-		})
-	}
-}
-
-func TestLearningPointsToLevel(t *testing.T) {
-	tc := []struct {
-		level      int
-		lowerLimit float32
-		upperLimit float32
-	}{
-		{level: 0, lowerLimit: 0, upperLimit: 1.9999},
-		{level: 1, lowerLimit: 2, upperLimit: 3.9999},
-		{level: 2, lowerLimit: 4, upperLimit: 7.9999},
-		{level: 3, lowerLimit: 8, upperLimit: 15.9999},
-		{level: 4, lowerLimit: 16, upperLimit: 31.9999},
-		{level: 5, lowerLimit: 32, upperLimit: 55.9999},
-		{level: 6, lowerLimit: 56, upperLimit: 87.9999},
-		{level: 7, lowerLimit: 88, upperLimit: 125.9999},
-		{level: 8, lowerLimit: 126, upperLimit: 169.9999},
-		{level: 9, lowerLimit: 170, upperLimit: 217.9999},
-		{level: 10, lowerLimit: 218, upperLimit: 273.9999},
-		{level: 11, lowerLimit: 274, upperLimit: 345.9999},
-		{level: 12, lowerLimit: 346, upperLimit: 433.9999},
-		{level: 13, lowerLimit: 434, upperLimit: 541.9999},
-		{level: 14, lowerLimit: 542, upperLimit: 673.9999},
-		{level: 15, lowerLimit: 674, upperLimit: 833.9999},
-		{level: 16, lowerLimit: 834, upperLimit: 1025.9999},
-		{level: 17, lowerLimit: 1026, upperLimit: 1253.9999},
-		{level: 18, lowerLimit: 1254, upperLimit: 1551.9999},
-		{level: 19, lowerLimit: 1552, upperLimit: 1833.9999},
-		{level: 20, lowerLimit: 1834, upperLimit: 9999},
-	}
-
-	for _, test := range tc {
-		t.Run(fmt.Sprintf("it converts lower limit threshold learning points %f to level %d", test.lowerLimit, test.level), func(t *testing.T) {
-			t.Parallel()
-
-			got := convertLearningPointsToLevel(test.lowerLimit)
-			if got != test.level {
-				t.Errorf("got %v, want %v", got, test.level)
-			}
-		})
-		t.Run(fmt.Sprintf("it converts upper limit threshold learning points %f to level %d", test.lowerLimit, test.level), func(t *testing.T) {
-			t.Parallel()
-
-			got := convertLearningPointsToLevel(test.upperLimit)
-			if got != test.level {
-				t.Errorf("got %v, want %v", got, test.level)
-			}
-		})
-	}
 }
