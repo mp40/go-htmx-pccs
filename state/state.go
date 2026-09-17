f1a9901a677f85bec9976b0fda212bb90d40fdca
mp40 <philps.matthew@gmail.com>
2026-09-17 07:48:02 +0200

    get uniform state

--- state/state.go
+++ state/state.go
@@ -63,3 +63,17 @@ func (s *State) GetUniforms() ([]Uniform, error) {
 	}
 	return uniforms, err
 }
+
+func (s *State) GetUniformByID(uniformID int) (*Uniform, error) {
+	uniform := Uniform{}
+	err := s.db.QueryRow("SELECT id, name, weight FROM uniform WHERE id = ?", uniformID).
+		Scan(&uniform.ID, &uniform.Name, &uniform, uniform.Weight)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &uniform, err
+}
